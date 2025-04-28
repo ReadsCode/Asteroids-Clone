cbdc9c29f3312d0aee297dd0d581f78769968af1
Ryan_E <Reads9728@gmail.com>
2025-04-28 18:59:45 -0400

    Create Framerate Limiter

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from constants import *
 
 pygame.init()
 
+clock = pygame.time.Clock()
+dt = 0
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -14,6 +17,7 @@ def main():
     while 0 == 0:
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
                return
