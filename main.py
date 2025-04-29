18bed8c158e76741742a14d13329c7a42b053f80
Ryan_E <Reads9728@gmail.com>
2025-04-29 18:44:12 -0400

    Added Turn Function for Player Sprite

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from player import *
 pygame.init()
 
 clock = pygame.time.Clock()
-dt = 0
 
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -16,9 +15,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     while 0 == 0:
         screen.fill((0,0,0))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         for event in pygame.event.get():
