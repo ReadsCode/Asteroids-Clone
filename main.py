66a7483a952f32a003c5561fb09a2be0b89142b4
Ryan_E <Reads9728@gmail.com>
2025-04-30 19:44:31 -0400

    added asteroid fields

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
 from player import *
+from asteroids import *
+from asteroidfield import *
+
 
 pygame.init()
+
 
 clock = pygame.time.Clock()
 
@@ -14,14 +18,28 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+
     dt = 0
+
     while 0 == 0:
         screen.fill((0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
                return
